b493f1e6fcd6470145760144f21217b70affd923
HP <movidamalani@gmail.com>
2022-11-04 17:02:24 +0700

    ok lagi

--- src/JawabanNoTiga.java
+++ src/JawabanNoTiga.java
@@ -2,6 +2,7 @@ public class JawabanNoTiga {
     public static void main(String[]args) {
         System.out.println("hello kelas 1e ");
         System.out.println("Yuhuuuuuuuu");
+        System.out.println("Yoiiiii");
     }
 
 }
