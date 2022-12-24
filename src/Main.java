88c29c5755ac61c8b734b447b74a4a074d785478
HP <movidamalani@gmail.com>
2022-12-24 19:26:17 +0700

    ready lagi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<int[]> StopGo = new ArrayList<int[]>();
+        StopGo.add(new int[]{10, 0});
+        StopGo.add(new int[]{3, 5});
+        StopGo.add(new int[]{2, 5});
+        System.out.println(GetPassanger(StopGo));
     }
+    public static int GetPassanger(ArrayList<int[]>BisKota){
+        System.out.println(5);
+        return 1*5;
+    }
+
 }
